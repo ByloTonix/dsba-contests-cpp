b861e853221f71c33db4abce1c3c044a577441cd
ByloTonix <bylotonix@gmail.com>
2024-03-16 07:01:20 +0300

    solutions

--- contest_cpp_5/D. Rational number.cpp
+++ contest_cpp_5/D. Rational number.cpp
@@ -1,4 +1,3 @@
-#include <cmath>
 #include <iostream>
 #include <stdexcept>
 
@@ -24,214 +23,166 @@ private:
         auto g = gcd(Numerator < 0 ? -Numerator : Numerator, Denominator);
         Numerator /= g;
         Denominator /= g;
+        if (Denominator < 0)
+        {
+            Numerator *= -1;
+            Denominator *= -1;
+        }
     }
 
 public:
-    // Конструкторы
-    Rational(int Numerator = 0, int Denominator = 1);
-    Rational(const Rational& other);
-
-    // Перегрузка операторов
-    const Rational& operator=(const Rational& other);
-    Rational operator+(const Rational& other) const;
-    Rational operator-(const Rational& other) const;
-    Rational operator*(const Rational& other) const;
-    Rational operator/(const Rational& other) const;
-
-    Rational operator+=(const Rational& other);
-    Rational operator-=(const Rational& other);
-    Rational operator*=(const Rational& other);
-    Rational operator/=(const Rational& other);
-
-    bool operator==(const Rational& other) const;
-    bool operator!=(const Rational& other) const;
-
-    Rational operator+() const;
-    Rational operator-() const;
-
-    Rational operator++();
-    Rational operator++(int);
-    Rational operator--();
-    Rational operator--(int);
-
-    int numerator() const { return Numerator; }
-    int denominator() const { return Denominator; }
-
-    // Rational operator+(int value) const
-    // {
-    //     return Rational(Numerator + value * Denominator, Denominator);
-    // }
-
-    // friend Rational operator+(int value, const Rational& rational)
-    // {
-    //     return rational + value;
-    // }
-
-    Rational operator-(int value) const
+    Rational(int n = 0, int d = 1) : Numerator(n), Denominator(d)
     {
-        return Rational(Numerator - value * Denominator, Denominator);
+        if (Denominator == 0)
+        {
+            throw std::invalid_argument("Denominator cannot be zero");
+        }
+        reducer();
     }
 
-    friend Rational operator-(int value, const Rational& rational)
+    Rational(const Rational& other) : Numerator(other.Numerator), Denominator(other.Denominator) {}
+
+    const Rational& operator=(const Rational& other)
     {
-        return Rational(value * rational.Denominator - rational.Numerator, rational.Denominator);
+        Numerator = other.Numerator;
+        Denominator = other.Denominator;
+        return *this;
     }
 
-    Rational operator*(int value) const
+    Rational operator+(const Rational& other) const
     {
-        return Rational(Numerator * value, Denominator);
+        return Rational(Numerator * other.Denominator + other.Numerator * Denominator,
+                        Denominator * other.Denominator);
     }
 
-    friend Rational operator*(int value, const Rational& rational)
+    Rational operator-(const Rational& other) const
     {
-        return rational * value;
+        return *this + Rational(-other.Numerator, other.Denominator);
     }
 
-    Rational operator/(int value) const
+    Rational operator*(const Rational& other) const
     {
-        return Rational(Numerator, Denominator * value);
+        return Rational(Numerator * other.Numerator, Denominator * other.Denominator);
     }
 
-    friend Rational operator/(int value, const Rational& rational)
+    Rational operator/(const Rational& other) const
     {
-        return Rational(value * rational.Denominator, rational.Numerator);
+        if (other.Numerator == 0)
+        {
+            throw std::invalid_argument("Division by ZERO");
+        }
+        return Rational(Numerator * other.Denominator, Denominator * other.Numerator);
     }
 
-    // friend дает доступ к private class
-    friend std::ostream& operator<<(std::ostream& out, const Rational& c);
+    Rational operator+=(const Rational& other)
+    {
+        *this = *this + other;
+        return *this;
+    }
 
-    friend Rational operator*(int left, const Rational& right);// Умножение на целое число
+    Rational operator-=(const Rational& other)
+    {
+        *this = *this - other;
+        return *this;
+    }
+
+    Rational operator*=(const Rational& other)
+    {
+        *this = *this * other;
+        return *this;
+    }
+
+    Rational operator/=(const Rational& other)
+    {
+        *this = *this / other;
+        return *this;
+    }
+
+    bool operator==(const Rational& other) const
+    {
+        return (Numerator == other.Numerator && Denominator == other.Denominator);
+    }
+
+    bool operator!=(const Rational& other) const
+    {
+        return !(*this == other);
+    }
+
+    Rational operator+() const
+    {
+        return *this;
+    }
+
+    Rational operator-() const
+    {
+        return Rational(-Numerator, Denominator);
+    }
+
+    Rational& operator++()
+    {
+        Numerator += Denominator;
+        reducer();
+        return *this;
+    }
+
+    Rational operator++(int)
+    {
+        Rational temp = *this;
+        ++(*this);
+        return temp;
+    }
+
+    Rational& operator--()
+    {
+        Numerator -= Denominator;
+        reducer();
+        return *this;
+    }
+
+    Rational operator--(int)
+    {
+        Rational temp = *this;
+        --(*this);
+        return temp;
+    }
+
+    int numerator() const
+    {
+        return Numerator;
+    }
+
+    int denominator() const
+    {
+        return Denominator;
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const Rational& c)
+    {
+        out << c.numerator() << "/" << c.denominator();
+        return out;
+    }
 };
 
-std::ostream& operator<<(std::ostream& out, const Rational& c)
+Rational operator+(int value, const Rational& rational)
 {
-    out << c.numerator() << "/" << c.denominator();
-    return out;
+    return rational + value;
 }
 
-// Реализация методов класса Rational
-
-Rational::Rational(int Numerator, int Denominator) : Numerator(Numerator), Denominator(Denominator) {reducer();}
-Rational::Rational(const Rational& other)
+Rational operator-(int value, const Rational& rational)
 {
-    Numerator = other.Numerator;
-    Denominator = other.Denominator;
+    return Rational(value * rational.denominator() - rational.numerator(), rational.denominator());
 }
 
-// Оператор присваивания
-const Rational& Rational::operator=(const Rational& other)
+Rational operator*(int value, const Rational& rational)
 {
-    Numerator = other.Numerator;
-    Denominator = other.Denominator;
-    return *this;
+    return rational * value;
 }
 
-// Оператор сложения
-Rational Rational::operator+(const Rational& other) const {        
-    return Rational(Numerator * other.Denominator + other.Numerator * Denominator, Denominator * other.Denominator);
-}
-
-// Оператор вычитания
-Rational Rational::operator-(const Rational& other) const
+Rational operator/(int value, const Rational& rational)
 {
-    return -other + *this;
-}
-
-// Оператор умножения
-Rational Rational::operator*(const Rational& other) const
-{
-    return Rational(Numerator * other.Numerator, Denominator * other.Denominator);
-}
-
-// Оператор деления
-Rational Rational::operator/(const Rational& other) const
-{
-    return Rational(Numerator * other.Denominator, Denominator * other.Numerator);
-}
-
-// Оператор сравнения на равенство
-bool Rational::operator==(const Rational& other) const
-{
-    return (Numerator == other.Numerator && Denominator == other.Denominator);
-}
-
-// Оператор сравнения на неравенство
-bool Rational::operator!=(const Rational& other) const
-{
-    return !(*this == other);
-}
-
-// Унарный оператор плюс
-Rational Rational::operator+() const
-{
-    return *this;
-}
-
-// Унарный оператор минус
-Rational Rational::operator-() const
-{
-    return Rational(-Numerator, Denominator);
-}
-
-Rational Rational::operator+=(const Rational& other)
-{
-    *this = *this + other;
-    return *this;
-}
-
-Rational Rational::operator-=(const Rational& other)
-{
-    *this = *this - other;
-    return *this;
-}
-
-Rational Rational::operator*=(const Rational& other)
-{
-    *this = *this * other;
-    return *this;
-}
-
-Rational Rational::operator/=(const Rational& other)
-{
-    *this = *this / other;
-    return *this;
-}
-
-
-Rational Rational::operator++()
-{
-    Numerator += Denominator;
-    reducer();
-    return *this;
-}
-
-Rational Rational::operator++(int)
-{
-    Rational temp = *this;
-    ++(*this);
-    return temp;
-}
-
-Rational Rational::operator--()
-{
-    Numerator -= Denominator;
-    reducer();
-    return *this;
-}
-
-Rational Rational::operator--(int)
-{
-    Rational temp = *this;
-    --(*this);
-    return temp;
-}
-
-int main() {
-    Rational r1 = (1, 2);
-    Rational r2 = (1, 2);
-    Rational r3 = r1 + r2;
-    std::cout << r3;
-    return 0;
-
-
+    if (rational.numerator() == 0)
+    {
+        throw std::invalid_argument("Division by ZERO");
+    }
+    return Rational(value * rational.denominator(), rational.numerator());
 }
---
+++ contest_cpp_5/E. Minesweeper.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+class Minesweeper {
+private:
+    size_t M, N;
+    vector<vector<int>> Table;
+
+public:
+    Minesweeper(size_t m, size_t n): M(m), N(n) {
+        Table.resize(M);
+        for (auto row : Table)
+            row.resize(N);
+    }
+
+    size_t Rows() {
+        return M;
+    }
+
+    size_t Columns() {
+        return N;
+    }
+    
+    void SetMine(size_t i, size_t j) {
+        Table[i][j] = -1;
+    }
+
+    int operator () (size_t i, size_t j) const {
+        return Table[i][j];
+    }
+
+    void CheckForMinesAround() {
+        for (size_t i = 0; i != M; ++i)
+            for (size_t j = 0; j != N; ++j)
+                CheckForMinesAround(i, j);
+    }
+
+private:
+    void CheckForMinesAround(size_t i, size_t j) {
+        int counter = 0;
+        for (int dx = -1; dx <= 1; ++dx) {
+            for (int dy = -1; dy <= 1; ++dy)
+                if (0 <= i + dx && i + dx < M &&
+                    0 <= j + dy && j + dy < N &&
+                    Table[i + dx][j + dy] == -1
+                ) {
+                    ++counter;
+                }
+        }
+        Table[i][j] = counter;
+    }
+};
+
+std::ostream& operator << (std::ostream& out, const Minesweeper& ms) {
+    for (size_t i = 0; i != ms.Rows(); ++i)
+        for (size_t j = 0; j != ms.Columns(); ++j)
+            if (ms(i, j) == -1)
+                out << '*';
+            else
+                out << ms(i, j);
+        out << "\n";
+    return out;
+}
